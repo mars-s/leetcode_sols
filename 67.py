5bb6b3c807f61897d9539f29a05a7e1e855b701c
Avinaba Das <71009876+mars-s@users.noreply.github.com>
2026-02-08 15:25:18 +1100

    init

---
+++ 67.py
@@ -0,0 +1,11 @@
+class Solution:
+    def addBinary(self, a: str, b: str) -> str:
+        len_a = len(a)
+        len_b: int = len(b)
+        
+        print(a[-1])
+        
+        # if len_a  > len_b:
+        #     a[::-1] == b[::-1]
+        # else: 
+        #     continue
